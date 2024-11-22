8266ce815451369e314f452c105e47da076b7dbc
LegnaOfc <Beatrizmanjarreztorres6@gmail.com>
2024-11-21 20:10:42 -0700

    Actualizar descargas-tiktok.js

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -1,69 +1,18 @@
-import fetch from 'node-fetch'
-import ffmpeg from "fluent-ffmpeg"
+ let handler = async (m, { conn, text, args, usedPrefix, command}) => {
+if (!args[0]) throw `✳️ ${mssg.noLink('TikTok')}\n\n 📌 ${mssg.example} : ${usedPrefix + command} https://vm.tiktok.com/ZMreHF2dC/`;
 
-var handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) {
-        throw m.reply(`*🌸 Ejemplo: ${usedPrefix + command
-        } https://vm.tiktok.com/ZMhAk8tLx/`);
-    }
+if (!/(?:https:?\/{2})?(?:w{3}|vm|vt|t)?\.?tiktok.com\/([^\s&]+)/gi.test(text)) throw `❎ ${mssg.noLink('TikTok')}`;
+await m.react(rwait)
 
-    try {
-        await conn.reply ( m.chat, "🌷 Espere un momento, estoy descargando su video...", m, );
+let data = await fg.tiktok(`${args[0]}`);
+let { title, play, duration } = data.result;
+let { nickname } = data.result.author;
 
-        const tiktokData = await tiktokdl(args[0]);
-
-        if (!tiktokData) {
-            throw m.reply("Error api!");
-        }
-
-        const videoURL = tiktokData.data.play;
-        const videoURLWatermark = tiktokData.data.wmplay;
-        const infonya_gan = `*📖 Descripción:* ${tiktokData.data.title}\n*🚀 Publicado:* ${tiktokData.data.create_time
-            }\n\n*⚜️ Estado:*\n=====================\nLikes = ${tiktokData.data.digg_count
-            }\nComentarios = ${tiktokData.data.comment_count}\nCompartidas = ${tiktokData.data.share_count
-            }\nVistas = ${tiktokData.data.play_count}\nDescargas = ${tiktokData.data.download_count
-            }\n=====================\n\nUploader: ${tiktokData.data.author.nickname || "No info"
-            }\n(${tiktokData.data.author.unique_id} - https://www.tiktok.com/@${tiktokData.data.author.unique_id
-            } )\n*🔊 Sonido:* ${tiktokData.data.music
-            }\n`;
-
-        if (videoURL || videoURLWatermark) {
-            await conn.sendFile( m.chat, videoURL, "tiktok.mp4", "`DESCARGA DE TIKTOK`"+`\n\n${infonya_gan}`, m, );
-            setTimeout(async () => {
-                //await conn.sendFile( m.chat, videoURLWatermark, "tiktokwm.mp4", `*Ini Versi Watermark*\n\n${infonya_gan}`, m, );
-                await conn.sendFile( m.chat, `${tiktokData.data.music}`, "lagutt.mp3", "", m, );
-                //conn.reply( m.chat, "•⩊• Ini kak Videonya ૮₍ ˶ᵔ ᵕ ᵔ˶ ₎ა\nDitonton yah ₍^ >ヮ<^₎", m, );
-            }, 1500);
-        } else {
-            throw m.reply("No se pudo descargar.");
-        }
-    } catch (error1) {
-        conn.reply(m.chat, `Error: ${error1}`, m);
-    }
-};
-
-handler.help = ['tiktok'].map((v) => v + ' *<link>*')
-handler.tags = ['descargas']
-handler.command = /^t(t|iktok(d(own(load(er)?)?|l))?|td(own(load(er)?)?|l))$/i
-
-handler.disable = false
-handler.register = true
-handler.limit = true
-
+conn.sendFile(m.chat, `${play}`, `tiktok.mp4`, `*🌴 ${nickname}:  ${title}*\n *• ${duration}*`, m);
+m.react(done)
+}
+handler.help = ['tiktok']
+handler.tags = ['dl']
+handler.command = /^(tt|tiktok)(dl|nowm)?$/i
+handler.diamond = 4
 export default handler
-
-async function tiktokdl(url) {
-    let tikwm = `https://www.tikwm.com/api/?url=${url}?hd=1`
-    let response = await (await fetch(tikwm)).json()
-    return response
-}
-
-async function convertVideoToMp3(videoUrl, outputFileName) {
-    return new Promise((resolve, reject) => {
-        ffmpeg(videoUrl)
-            .toFormat("mp3")
-            .on("end", () => resolve())
-            .on("error", (err) => reject(err))
-            .save(outputFileName);
-    });
-}
